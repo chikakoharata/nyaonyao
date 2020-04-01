8caedfc17cfb39fcbb7a2d0fc768852c22755cb1
chikakoharata <59302207+chikakoharata@users.noreply.github.com>
2020-04-01 16:29:40 +0900

    投稿詳細・削除機能の実装 (#4)
    * 詳細ページの作成
    
    * 投稿削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i(show destroy)
+  
   def new
     @post = Post.new
   end
@@ -15,9 +17,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order('created_at DESC')
   end
-  
+
+  def show
+  end
+
+  def destroy
+    redirect_to root_path, notice: '削除しました' if @post.destroy
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body, :video)
+    end
+    
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
 end
