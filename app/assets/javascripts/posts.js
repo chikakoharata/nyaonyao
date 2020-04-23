8adce6acca44144de895e318afb4b1bbb11f47a8
harata <c.oki1216@gmail.com>
2020-04-23 16:43:07 +0900

    Update posts.js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,20 +1,20 @@
 $(function () {
-	      $(document).on('click', '#comment_button', function () {
-                var comment = $("#comment_text").val();
-                var commentPos = $('#nicoscreen').offset();
-                var nico_comment = $('<div id="item" style="font-size: 36px; color: white; font-weight: bold; text-shadow: black 1px 1px 1px; width: 100px; white-space: nowrap; position: absolute;">' + comment + '</li>').appendTo('#nicoscreen');
-                var offset = nico_comment.offset({ top: setPosition('#item'), left: commentPos.left + $('#nicoscreen').width() });
-                var animation = offset.animate({ left: commentPos.left }, {
-                        duration: 3000,
-                        easing: 'linear',
-                        queue: false,
-                        complete: function () {
-                                animation.hide();
-                        }
-                });
-                $('<li id="list_item" class="list-group-item list-group-item-light" color:black">' + comment + '</li>').prependTo('#list');
-                $("#comment_text").val("");
-	      });
+        $(document).on('click', '#comment_button', function () {
+        var comment = $("#comment_text").val();
+        var commentPos = $('#nicoscreen').offset();
+        var nico_comment = $('<div id="item" style="font-size: 36px; color: white; font-weight: bold; text-shadow: black 1px 1px 1px; width: 100px; white-space: nowrap; position: absolute;">' + comment + '</li>').appendTo('#nicoscreen');
+        var offset = nico_comment.offset({ top: setPosition('#item'), left: commentPos.left + $('#nicoscreen').width() });
+        var animation = offset.animate({ left: commentPos.left }, {
+                duration: 3000,
+                easing: 'linear',
+                queue: false,
+                complete: function () {
+                        animation.hide();
+                }
+        });
+        $('<li id="list_item" class="list-group-item list-group-item-light" color:black">' + comment + '</li>').prependTo('#list');
+        $("#comment_text").val("");
+        });
 });
 
 function setPosition(id) {
