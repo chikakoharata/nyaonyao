58bb84ee004655bbcb7eeb636cabcbe1eafa493e
harata <c.oki1216@gmail.com>
2020-04-24 18:14:57 +0900

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
 
-  resources :posts, only: %i(new create index show destroy)
+  resources :posts, only: %i(new create index show destroy) do
+    resources :likes, only: %i(create destroy)
+  end
 end
