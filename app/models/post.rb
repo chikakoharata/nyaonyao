5c08343ade158f50e6bee31cbe0c9f9756f57889
harata <c.oki1216@gmail.com>
2020-04-24 18:04:04 +0900

    func-like

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   validates :video, presence: true
   mount_uploader :video, VideoUploader
 end
