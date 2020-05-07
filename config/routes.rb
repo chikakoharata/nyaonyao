5c5bf3b4c8a7cd002f1134c46ba780771818904b
harata <c.oki1216@gmail.com>
2020-05-07 17:13:11 +0900

    registrationsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { registrations: 'registrations' }
+    controllers: { registrations: 'users/registrations' }
 
   root 'posts#index'
 
